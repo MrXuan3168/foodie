b6891effba3d1282225aaed17e58ba1dbbb06f23
主打情歌 <1004108488@qq.com>
2019-11-25 23:59:12 +0800

    1.通过AOP实现日志输出service执行时间

--- foodie-api/src/main/java/com/so/controller/HelloController.java
+++ foodie-api/src/main/java/com/so/controller/HelloController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
 import springfox.documentation.annotations.ApiIgnore;
 
 /**
@@ -17,9 +18,11 @@ import springfox.documentation.annotations.ApiIgnore;
  */
 @ApiIgnore
 @RestController
+@Slf4j
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
+        log.info("hello world");
         return "hello world";
     }
 
