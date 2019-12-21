f253a675727da1378747ccf73c69f431e8fb4f17
show <1004108488@qq.com>
2019-12-21 22:40:11 +0800

    1、优化返回值

--- foodie-dev/foodie-api/src/main/java/com/so/controller/OrdersController.java
+++ foodie-dev/foodie-api/src/main/java/com/so/controller/OrdersController.java
@@ -1,24 +1,22 @@
 package com.so.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.so.vo.MerchantOrdersVO;
-import com.so.vo.OrderVO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
 import com.so.bo.SubmitOrderBO;
 import com.so.enums.OrderStatusEnum;
 import com.so.enums.PayMethod;
 import com.so.service.OrderService;
 import com.so.utils.CookieUtils;
 import com.so.utils.Rest;
-
+import com.so.vo.MerchantOrdersVO;
+import com.so.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 应用模块名称：订单</br>
@@ -41,7 +39,7 @@ public class OrdersController extends BaseController {
 
     @ApiOperation(value = "用户下单", notes = "用户下单", httpMethod = "POST")
     @PostMapping("/create")
-    public Rest create(@RequestBody SubmitOrderBO bo) {
+    public Rest<String> create(@RequestBody SubmitOrderBO bo) {
         Integer payMethod = bo.getPayMethod();
         if (!PayMethod.WE_CHAT.type.equals(payMethod) && !PayMethod.ALI_PAY.type.equals(payMethod)) {
             return Rest.errorMsg("支付方式不支持");
