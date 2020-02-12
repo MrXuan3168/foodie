0fcd431b8b5a2dd04162baff2413df9d66f03223
xuanweiyao <1004108488@qq.com>
2020-02-12 18:06:38 +0800

    test(测试jenkins打包):

--- foodie-dev/foodie-api/src/main/java/com/so/controller/HelloController.java
+++ foodie-dev/foodie-api/src/main/java/com/so/controller/HelloController.java
@@ -37,4 +37,5 @@ public class HelloController {
         session.removeAttribute("userInfo");
         return "ok";
     }
+
 }
---
+++ foodie-dev/foodie-api/src/main/java/com/so/utils/VersionUtil.java
@@ -0,0 +1,46 @@
+package com.so.utils;
+
+import java.io.*;
+
+import org.springframework.util.ResourceUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 版本号读取工具类
+ */
+@Slf4j
+public class VersionUtil {
+
+    private static final String VERSION_FILE_NAME = "classpath:version.txt";
+
+    /**
+     * @return 返回版本号
+     */
+    public static String getVersionText() {
+        StringBuilder result = new StringBuilder();
+        try {
+            File versionFile = ResourceUtils.getFile(VERSION_FILE_NAME);
+            if (versionFile.exists()) {
+                System.out.println(versionFile.getAbsolutePath());
+                try (BufferedReader br = new BufferedReader(new FileReader(versionFile))) {
+                    String s;
+                    while ((s = br.readLine()) != null) {
+                        result.append(System.lineSeparator()).append(s);
+                    }
+                } catch (IOException e) {
+                    log.error("io异常");
+                }
+
+            }
+        } catch (FileNotFoundException e) {
+            log.error("FileNotFoundException异常");
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("version:" + getVersionText());
+    }
+
+}
