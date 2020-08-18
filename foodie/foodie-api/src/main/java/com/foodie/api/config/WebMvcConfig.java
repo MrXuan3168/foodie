bfedaa949add5336f7ef90c8c8deb3bffba5e45d
jamie <1004108488@qq.com>
2020-08-18 09:38:26 +0800

    feat: 利用MOC在日志输出时添加唯一标识

--- foodie/foodie-api/src/main/java/com/foodie/api/config/WebMvcConfig.java
+++ foodie/foodie-api/src/main/java/com/foodie/api/config/WebMvcConfig.java
@@ -4,6 +4,9 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * 应用模块名称：
@@ -11,11 +14,32 @@ import org.springframework.web.client.RestTemplate;
  * @since 2020/8/11 23:32
  */
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
+    }
+
+    /**
+     * 把我们的拦截器注入为bean
+     * @return
+     */
+    @Bean
+    public HandlerInterceptor logInterceptor() {
+        return new LogInterceptor();
+    }
+
+    /**
+     * 注册拦截器
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // addPathPatterns 用于添加拦截规则, 这里假设拦截 /url 后面的全部链接
+        // excludePathPatterns 用户排除拦截
+        registry.addInterceptor(logInterceptor()).addPathPatterns("/**");
+        super.addInterceptors(registry);
     }
 
 }
