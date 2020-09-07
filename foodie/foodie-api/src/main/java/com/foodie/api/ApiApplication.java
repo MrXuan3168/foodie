98e67c632d08460997d63f65f448a0ce2e39b16a
jamie <1004108488@qq.com>
2020-09-07 23:01:23 +0800

    feat: 引入SpringSession实现用户会话

--- foodie/foodie-api/src/main/java/com/foodie/api/ApiApplication.java
+++ foodie/foodie-api/src/main/java/com/foodie/api/ApiApplication.java
@@ -3,10 +3,12 @@ package com.foodie.api;
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
@@ -14,14 +16,16 @@ import tk.mybatis.spring.annotation.MapperScan;
  * MapperScan 扫描mapper所在包
  * ComponentScan 扫描所有包及相关注解包
  * EnableScheduling 开启定时任务
+ * EnableRedisHttpSession 开启使用redis作为spring session
  * @author jamie
  * @since 2019/11/12 15:48
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @MapperScan(basePackages = "com.foodie.mapper")
 @ComponentScan(basePackages = {"com.foodie", "org.n3r.idworker"})
 @EnableKnife4j
 @EnableScheduling
+@EnableRedisHttpSession
 public class ApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
@@ -32,7 +36,6 @@ public class ApiApplication extends SpringBootServletInitializer {
      * 为了打包springboot项目
      */
     @Override
-
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(this.getClass());
     }
